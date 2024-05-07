dc9890c615aebcd0254e3de5083d4eb19122fdc9
foxr-lolunwer <116154695+foxr-lolunwer@users.noreply.github.com>
2024-05-07 21:01:38 +0800

    Create GenerateBattery.py
    requires key

---
+++ GenerateBattery.py
@@ -0,0 +1,393 @@
+# DD
+# category = 'dd'
+# capital_ship = False
+# hg_attack = 0
+# hg_attack_up = 0
+# # 76mm
+# key_mm = 76
+# hg_attack = 0
+# hg_attack_up = 0
+# lg_attack = 0.5
+# lg_attack_up = 0.3
+# anti_air_attack = 0.6
+# anti_air_attack_up = 0.4
+# lg_armor_piercing = 0.6
+# lg_armor_piercing_up = 0.2
+# naval_speed = -0.01
+# build_cost_ic = 30
+# build_cost_ic_up = 10
+# # 102mm
+# key_mm = 102
+# lg_attack = 0.7
+# lg_attack_up = 0.35
+# anti_air_attack = 0.8
+# anti_air_attack_up = 0.45
+# lg_armor_piercing = 0.75
+# lg_armor_piercing_up = 0.3
+# naval_speed = -0.015
+# build_cost_ic = 40
+# build_cost_ic_up = 15
+# # 127mm
+# key_mm = 127
+# lg_attack = 0.9
+# lg_attack_up = 0.4
+# anti_air_attack = 1
+# anti_air_attack_up = 0.5
+# lg_armor_piercing = 0.8
+# lg_armor_piercing_up = 0.4
+# naval_speed = -0.02
+# build_cost_ic = 50
+# build_cost_ic_up = 20
+# # 152mm-dd
+# key_mm = 152
+# lg_attack = 1.2
+# lg_attack_up = 0.5
+# anti_air_attack = 1.4
+# anti_air_attack_up = 0.6
+# lg_armor_piercing = 1
+# lg_armor_piercing_up = 0.5
+# naval_speed = -0.03
+# build_cost_ic = 70
+# build_cost_ic_up = 30
+# #######################################################
+# # CL
+# category = 'cl'
+# capital_ship = False
+# # 152mm
+# key_mm = 152
+# hg_attack = 0
+# hg_attack_up = 0
+# lg_attack = 1.4
+# lg_attack_up = 0.6
+# anti_air_attack = 1.4
+# anti_air_attack_up = 0.6
+# lg_armor_piercing = 1.5
+# lg_armor_piercing_up = 0.8
+# naval_speed = -0.01
+# build_cost_ic = 70
+# build_cost_ic_up = 30
+# # 180mm
+# key_mm = 180
+# lg_attack = 1.8
+# lg_attack_up = 0.8
+# hg_attack = 0.05
+# hg_attack_up = 0.05
+# anti_air_attack = 1.4
+# anti_air_attack_up = 0.6
+# lg_armor_piercing = 1.8
+# lg_armor_piercing_up = 1
+# naval_speed = -0.01
+# build_cost_ic = 90
+# build_cost_ic_up = 40
+# # 203mm-cl
+# key_mm = 203
+# lg_attack = 2.1
+# lg_attack_up = 1
+# hg_attack = 0.15
+# hg_attack_up = 0.1
+# anti_air_attack = 1.5
+# anti_air_attack_up = 0.6
+# lg_armor_piercing = 2.4
+# lg_armor_piercing_up = 2.4
+# naval_speed = -0.02
+# build_cost_ic = 120
+# build_cost_ic_up = 60
+# #######################################################
+# # CA
+# # 203mm
+# key_mm = 203
+# category = 'ca'
+# capital_ship = False
+# lg_attack = 2.0
+# lg_attack_up = 0.5
+# hg_attack = 0.2
+# hg_attack_up = 0.15
+# anti_air_attack = 1.2
+# anti_air_attack_up = 0.4
+# lg_armor_piercing = 4.0
+# lg_armor_piercing_up = 2.0
+# naval_speed = -0.02
+# build_cost_ic = 100
+# build_cost_ic_up = 50
+# # 233mm
+# key_mm = 233
+# category = 'ca'
+# capital_ship = None
+# lg_attack = 1.5
+# lg_attack_up = 0.2
+# hg_attack = 0.4
+# hg_attack_up = 0.3
+# anti_air_attack = 1.0
+# anti_air_attack_up = 0.3
+# lg_armor_piercing = 5.0
+# lg_armor_piercing_up = 2.5
+# naval_speed = -0.025
+# build_cost_ic = 150
+# build_cost_ic_up = 100
+# # 282mm-ca
+# key_mm = 282
+# category = 'ca'
+# capital_ship = None
+# lg_attack = 1.0
+# lg_attack_up = 0.2
+# hg_attack = 0.8
+# hg_attack_up = 0.6
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 7.0
+# lg_armor_piercing_up = 3.5
+# naval_speed = -0.03
+# build_cost_ic = 300
+# build_cost_ic_up = 150
+# #######################################################
+# # CA
+# # 282mm
+# key_mm = 282
+# category = 'cb'
+# capital_ship = True
+# lg_attack = 0.8
+# lg_attack_up = 0.1
+# hg_attack = 1
+# hg_attack_up = 0.6
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 8
+# lg_armor_piercing_up = 3
+# naval_speed = -0.02
+# build_cost_ic = 350
+# build_cost_ic_up = 200
+# # 305mm
+# key_mm = 305
+# category = 'cb'
+# capital_ship = True
+# lg_attack = 0.8
+# lg_attack_up = 0.15
+# hg_attack = 1.5
+# hg_attack_up = 0.6
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 10
+# lg_armor_piercing_up = 4
+# naval_speed = -0.03
+# build_cost_ic = 400
+# build_cost_ic_up = 300
+# # 351mm
+# key_mm = 351
+# category = 'cb'
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.25
+# hg_attack = 2.0
+# hg_attack_up = 0.9
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 12
+# lg_armor_piercing_up = 6
+# naval_speed = -0.03
+# build_cost_ic = 600
+# build_cost_ic_up = 300
+# #######################################################
+# # BB/BC
+# category = 'bb'  # bb/bc
+# capital_ship = True
+# # 305mm
+# key_mm = 305
+# lg_attack = 0.6
+# lg_attack_up = 0.2
+# hg_attack = 1.8
+# hg_attack_up = 0.8
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 12
+# lg_armor_piercing_up = 6
+# naval_speed = -0.025
+# build_cost_ic = 500
+# build_cost_ic_up = 250
+# # 351mm
+# key_mm = 351
+# category = 'bb'  # bb/bc
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.25
+# hg_attack = 2.2
+# hg_attack_up = 1
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 15
+# lg_armor_piercing_up = 8
+# naval_speed = -0.03
+# build_cost_ic = 600
+# build_cost_ic_up = 300
+# # 380mm
+# key_mm = 380
+# category = 'bb'  # bb/bc
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.3
+# hg_attack = 2.6
+# hg_attack_up = 1.2
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 25
+# lg_armor_piercing_up = 6
+# naval_speed = -0.035
+# build_cost_ic = 700
+# build_cost_ic_up = 350
+# # 420mm
+# key_mm = 420
+# category = 'bb'  # bb/bc
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.4
+# hg_attack = 3
+# hg_attack_up = 2
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 30
+# lg_armor_piercing_up = 6
+# naval_speed = -0.03875
+# build_cost_ic = 850
+# build_cost_ic_up = 425
+# # 457mm
+# key_mm = 457
+# category = 'bb'  # bb/bc
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.45
+# hg_attack = 5
+# hg_attack_up = 2
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 35
+# lg_armor_piercing_up = 6
+# naval_speed = -0.0425
+# build_cost_ic = 1000
+# build_cost_ic_up = 500
+# #######################################################
+# # SHBB
+# # 480mm
+# key_mm = 480
+# category = 'shbb'
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.5
+# hg_attack = 10
+# hg_attack_up = 3
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 50
+# lg_armor_piercing_up = 5
+# naval_speed = -0.04
+# build_cost_ic = 1250
+# build_cost_ic_up = 675
+# 510mm
+# key_mm = 510
+# category = 'shbb'
+# capital_ship = True
+# lg_attack = 0.6
+# lg_attack_up = 0.6
+# hg_attack = 14
+# hg_attack_up = 3
+# anti_air_attack = 0
+# anti_air_attack_up = 0
+# lg_armor_piercing = 60
+# lg_armor_piercing_up = 8
+# naval_speed = -0.05
+# build_cost_ic = 1500
+# build_cost_ic_up = 750
+# -1*7
+# 76 key in list: -1 10 20 -1 11 21 30 -1 12 22 31 020 030 -1 32 40 021 031 -1 41 50 022 032 040 -1 42 51 041 -1 52 042
+# 102 key in list: -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 40 020 030 -1 41 50 021 031 -1 42 51 022 032 040 -1 52 041
+# 127 key in list: -1 -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 40 020 030 -1 41 50 021 031 -1 42 51 022 032
+# 152 dd key in list: -1 -1 -1 10 20 30 -1 11 21 31 -1 12 22 32 021 031 -1 021 031 -1 022 032
+# 152 key in list: -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 020 030 -1 40 021 031  -1 41 022 032 040 -1 42 041
+# 180 key in list: -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 -1 020 030  -1 41 021 031 40 -1 022 032 41
+# 203 cl key in list: -1 -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 -1 40 -1 41
+# 203 key in list: -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 40 -1 41 -1 42
+# 233 key in list: -1 10 20 -1 11 21 30 -1 12 22 31 -1 32 -1 40 -1 41
+# 282 ca key in list: -1 -1 10 20 -1 11 21 -1 12 22 30 -1 31 -1 32 -1 40
+# 282 key in list: -1 -1 10 20 -1 11 21 -1 12 22 30 -1 31 -1 32 -1 40
+# 305 cb key in list: -1 20 -1 21 30 -1 22 31 -1 32 40 -1 41 50 -1 42 51 -1 52
+# 351 cb key in list: -1 20 -1 21 30 -1 22 31 -1 32 40 -1 41 -1 42 50 -1 51
+# 305 key in list: -1 20 -1 21 30 -1 22 31 -1 32
+# 351 key in list: -1 20 -1 21 30 -1 22 31 -1 32
+# 380 key in list: -1 20 -1 21 30 -1 22 31 -1 32 40 -1 41 -1 42 50 -1 51
+# 420 key in list: -1 -1 20 -1 21 -1 22 30 -1 31 40 -1 32 41 -1 42
+# 457 key in list: -1 -1 -1 20 -1 21 30 -1 22 31 -1 32 41 -1 42
+# 480 key in list: -1 -1 -1 -1 20 -1 21 30 -1 22 31 -1 32 41
+# 510 key in list: -1 -1 -1 -1 -1 20 -1 21 30 -1 22 31
+
+out_text = ''
+year_list = ['wwi', 'interwar', 'preparation', 'year36', 'year39', 'year42', 'year45', 'year48']
+num_key_list = input("put:").split()
+# count = int(num_key[1])
+# lian = int(num_key[-2])
+# lve = int(num_key[-1])
+year_count = 0
+for num_key in num_key_list:
+    if num_key == '-1':
+        out_text += '    # ' + year_list[year_count] + '\n'
+        year_count += 1
+        continue
+    count = 1
+    lian = int(num_key[-2])
+    lve = int(num_key[-1])
+    if num_key[0] == '0':
+        lg_attack_up *= 0.8
+        hg_attack_up *= 0.8
+        lg_armor_piercing_up *= 0.8
+        hg_attack_up *= 0.5
+        build_cost_ic *= 1.2
+
+    count_fact = (1 - count / 20) * count
+    lg_lv_add = lg_attack_up + lg_attack_up * (lve - 1) / 1.2
+    hg_lv_add = hg_attack_up + hg_attack_up * (lve - 1) / 1.2
+    anti_lv_add = anti_air_attack_up + anti_air_attack_up * (lve - 1) / 1.2
+    armor_piercing_lv_add = lg_armor_piercing_up + lg_armor_piercing_up * (lve - 1) / 1.2
+    lian_fact = lian / 1.2
+    weigh_fact = count * (lian / 1.5)
+    this_lg_attack = 0
+    this_hg_attack = 0
+    this_lg_armor_piercing = 0
+    this_build_cost_ic = 0
+    this_anti_air_attack = 0
+    this_naval_speed = 0
+    this_lg_attack += (lg_attack + lg_lv_add) * count_fact * lian_fact
+    this_hg_attack += (hg_attack + hg_lv_add) * count_fact * lian_fact
+    this_anti_air_attack += (anti_air_attack + anti_lv_add) * count_fact * lian_fact
+    this_naval_speed += naval_speed * weigh_fact * 0.6
+    i_lg_armor_piercing = (lg_armor_piercing + armor_piercing_lv_add) * (1 + lian / 10)
+    if i_lg_armor_piercing > this_lg_armor_piercing:
+        this_lg_armor_piercing = i_lg_armor_piercing
+    this_build_cost_ic = (build_cost_ic + build_cost_ic_up * lve) * weigh_fact
+
+    out_text += "    ship_battery_%dmm_with%s_%s = {" % (key_mm, num_key[:-1], num_key[-1]) + '\n'
+    out_text += "        # the key is:" + num_key + '\n'
+    out_text += '        category = ship_%s_%smm_battery' % (category, key_mm) + '\n'
+    out_text += '        sfx = sfx_ui_sd_module_turret' + '\n'
+    if capital_ship:
+        out_text += '        add_equipment_type = capital_ship' + '\n'
+    out_text += '        add_stats = {' + '\n'
+    out_text += '            lg_attack = %.1f' % this_lg_attack + '\n'
+    if this_hg_attack:
+        out_text += '            hg_attack = %.1f' % this_hg_attack + '\n'
+    out_text += '            build_cost_ic = %d' % int(this_build_cost_ic) + '\n'
+    if num_key[0] == '0':
+        out_text += '            anti_air_attack = %.1f' % this_anti_air_attack + '\n'
+    out_text += '        }' + '\n'
+    out_text += '        multiply_stats = {' + '\n'
+    out_text += '            naval_speed = %.2f' % this_naval_speed + '\n'
+    out_text += '        }' + '\n'
+    out_text += '        add_average_stats = {' + '\n'
+    out_text += '            lg_armor_piercing = %.2f' % this_lg_armor_piercing + '\n'
+    if this_hg_attack:
+        out_text += '            hg_armor_piercing = %.2f' % this_lg_armor_piercing + '\n'
+    out_text += '        }' + '\n'
+    out_text += '        can_convert_from = {' + '\n'
+    out_text += '            module_category = ship_%s_%smm_battery' % (category, key_mm) + '\n'
+    out_text += '            convert_cost_ic = %d' % int(this_build_cost_ic * 0.8) + '\n'
+    out_text += '        }' + '\n'
+    out_text += '        critical_parts = {damaged_heavy_guns}' + '\n'
+    out_text += "    }" + '\n'
+with open(out_file_path, "w", encoding="utf-8") as f:
+    f.write(out_text)
